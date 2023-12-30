ed08a85f0f15bbc1d1ea0bf2304ec6d954aa7d46
eclopez <erik@eriklopez.net>
2023-12-29 21:37:30 -0700

    feat: Add home page

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next Starter Kit",
+  description: "Starting point for Next.js projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
